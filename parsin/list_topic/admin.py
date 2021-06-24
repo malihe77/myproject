69e457a9ae8f98cfaaf53cada00e27ad1ff03983
malihe77 <malihe7713@gmail.com>
2021-06-24 20:52:09 +0430

    first my site

---
+++ parsin/list_topic/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin 
+from .models import Post
+@admin.register(Post) 
+class PostAdmin(admin.ModelAdmin):    
+    list_display = ('research_title', 'slug', 'author', 'publish', 'status') 
+    list_filter = ('status', 'created', 'publish', 'author')    
+    search_fields = ('research_title', 'Research_content')    
+    prepopulated_fields = {'slug': ('research_title',)}    
+    raw_id_fields = ('author',)    
+    date_hierarchy = 'publish'    
+    ordering = ('status', 'publish') 
